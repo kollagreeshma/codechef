861f929ea105d6ca88a9cbc7de0d37f4fc1c46d4
kollagreeshma <kollagreeshmasree2@gmail.com>
2024-12-09 16:09:49 +0530

    Add files via upload

---
+++ cricketworldcupqualifier.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class cricket
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		if(x>=12)
+		System.out.println("yes");
+		else
+		System.out.println("No");
+	}
+
+}
