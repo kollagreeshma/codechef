774326bbf97a858c8d97d7d5d597229c2257750d
kollagreeshma <kollagreeshmasree2@gmail.com>
2024-12-09 16:09:12 +0530

    Add files via upload

---
+++ chodechef.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class co
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		if(x + y>= a)
+		{
+		System.out.println("Yes");
+		}
+		else
+		{
+		System.out.println("No");
+		}
+	}
+}
