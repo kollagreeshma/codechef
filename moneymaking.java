25ab24f158a46fc13ccf414888820559222cf3dc
kollagreeshma <kollagreeshmasree2@gmail.com>
2024-12-09 16:10:50 +0530

    Add files via upload

---
+++ moneymaking.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class moneymaking
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		int sell=x*5000+y*9800;
+		System.out.println(sell);
+
+	}
+}
