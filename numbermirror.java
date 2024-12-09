4c88c1d56b1d53ba095a3d2b087b5ad69ece3bcd
kollagreeshma <kollagreeshmasree2@gmail.com>
2024-12-09 16:11:11 +0530

    Add files via upload

---
+++ numbermirror.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+class mirror
+{
+	public static void main (String[] args)
+	{
+	    // Do not print anything here before taking input
+	    Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		// Print the value of n
+		System.out.println(n);
+		
+		
+	}
+}
