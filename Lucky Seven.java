71b1c43edc46a6aa336fb099134ded0135bcb6d6
Kolla Greeshma Sree <kollagreeshmasree2@gmail.com>
2024-12-14 16:04:43 +0530

    Add files via upload

---
+++ Lucky Seven.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+	Scanner n=new Scanner(System.in);
+	String S=n.nextLine();
+	char luckynumber=S.charAt(6);
+	System.out.println(luckynumber);
+
+	}
+}
