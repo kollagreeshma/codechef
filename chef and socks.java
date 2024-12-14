b322d40a2d124c85bcc9d7efe105833580f342fc
Kolla Greeshma Sree <kollagreeshmasree2@gmail.com>
2024-12-14 15:40:30 +0530

    Add files via upload

---
+++ chef and socks.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(string args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        if(x+y>=a){
+            System.out.println("yes");
+        else
+        {
+            System.out.println("No");
+        }
+        }
+    }
+}
