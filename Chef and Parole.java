506b22fec26a926a1e8958dffc2c1c8ad3fb2ea4
Kolla Greeshma Sree <kollagreeshmasree2@gmail.com>
2024-12-15 12:04:48 +0530

    Add files via upload

---
+++ Chef and Parole.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		if(x>=7)
+		{
+		    System.out.println("Yes");
+		}
+		else
+        {
+            System.out.println("No");
+        }
+	}
+}
