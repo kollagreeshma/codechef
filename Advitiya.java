7dafd25541cdf5f80f9329e1e2555d17746b7435
Kolla Greeshma Sree <kollagreeshmasree2@gmail.com>
2024-12-15 12:12:04 +0530

    Add files via upload

---
+++ Advitiya.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n>=16&&n<=18)
+		{
+		    System.out.println("ADVITIYA");
+		}
+		else
+		{
+		    System.out.println("WAITING FOR ADVITIYA");
+		}
+
+	}
+}
